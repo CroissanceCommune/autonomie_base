87502dcf628f450ae71d7e791d12328fc1d77694
G.Tjebbes <g.t@majerti.fr>
2017-07-10 15:04:17 +0200

    Add constants

---
+++ autonomie_base/consts.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+SEX_OPTIONS = (
+    ('', '', ),
+    ('M', 'Homme', ),
+    ('F', 'Femme', ),
+)
+
+
+CIVILITE_OPTIONS = (
+    ('', u'Non renseigné', ),
+    ('Monsieur', u'Monsieur',),
+    ('Madame', u'Madame',),
+)
