4e726418d61f40fa832098de757446b9786a7de4
G.Tjebbes <g.t@majerti.fr>
2017-08-11 14:48:15 +0200

    Add modules

---
+++ autonomie_base/utils/export.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+
+def format_boolean(value):
+    """
+    Format a boolean value
+    """
+    return "Y" and value or "N"
---
+++ autonomie_base/utils/strings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# * Authors:
+#       * TJEBBES Gaston <g.t@majerti.fr>
+#       * Arezki Feth <f.a@majerti.fr>;
+#       * Miotte Julien <j.m@majerti.fr>;
+
+
+def format_quantity(quantity):
+    """
+        format the quantity
+    """
+    if quantity is not None:
+        return locale.format('%g', quantity, grouping=True)
+    else:
+        return ""
